335d8fb4f5b0ac0ffecdb21a9ee948fbb8b68a33
XlXi <wagnessgaming2014@gmail.com>
2020-12-14 00:01:53 -0500

    Incident ids

--- WebAssemblies/API.php
+++ WebAssemblies/API.php
@@ -16,9 +16,12 @@ class API {
 	}
 	
 	function InvokeError($errorCode, $message) {
+		$incident = md5(time()*8^42);
+		$incidentRand = ["azrt4o", "30jg49k", "nigm4gd"];
+		$incident = substr_replace($incident, $incidentRand[array_rand($incidentRand)], 15, 0);
 		http_response_code($errorCode);
 		header("Content-Type: application/json");
-		exit(API::PrettyJSONEncode(["error"=>"$message"]));
+		exit(API::PrettyJSONEncode(["error"=>"$message", "incidentID"=>$incident]));
 	}
 	
 	function PrettyJSONEncode($array){
