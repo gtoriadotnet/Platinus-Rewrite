a521113085c1cafc0792a8317cafe52755a7d15e
XlXi <wagnessgaming2014@gmail.com>
2021-01-10 04:54:54 -0500

    asdasda

--- public/api/versioncompatibility/allowedmd5hash.php
+++ public/api/versioncompatibility/allowedmd5hash.php
@@ -11,7 +11,7 @@ require $_SERVER["DOCUMENT_ROOT"] . "/../../../WebAssemblies/loader.php";
 $page = new Platinus\API();
 
 if(isset($_GET["apiKey"]) && $_GET["apiKey"]=="2b4ba7fc-5843-44cf-b107-ba22d3319dcd"){
-	$page->RespondJSON(array("data"=>["d4ade5b9e36fd63c3b27a2a747cc8d51"]));
+	$page->RespondJSON(array("data"=>["d4ade5b9e36fd63c3b27a2a747cc8d51", "e905f3e5c564421557ab77d93576f135"]));
 }else{
 	$page->InvokeError(403, "Unauthorized");
 }
